225030f47f6b884ad90a05eef194d5953481bb51
Abayomi Oyewumi <ace@syntaximagination.com>
2021-07-14 15:29:15 +0100

    Starting Application and also made it a global app

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+/**
+Name : Cli Gen for all sort of things
+Author:  Abayomi
+License : MIT
+Version : 1.0
+**/
+
+const aceUtils = require('./core/cli');
+console.log(__dirname);
+console.log(__filename);
+// aceUtils.init();
